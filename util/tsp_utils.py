991818c9e4bb17fc7be31463b0053ae54846fdc9
vchapple17 <vchapple17@gmail.com>
2017-11-21 10:16:48 -0600

    Add project Files from instructor

--- util/tsp_utils.py
+++ util/tsp_utils.py
@@ -13,24 +13,24 @@ class City:
         self.id = id
         self.x = int(x)
         self.y = int(y)
-    
+
     # returns city's id
     def getId(self):
         return self.id
-    
+
     # returns tuple with x,y coords of city
     def getCoords(self):
         return (self.x,self.y)
-    
+
     # accepts another city object and returns the euclidean
     # distance to that other city rounded to nearest int
     def distanceTo(self, otherCity):
         # (I did some testing on various methods of calculating this result (using numpy function, using pow(), etc)
         # This method was faster than the others.
         initialVal = math.sqrt((self.x - otherCity.x)**2 + (self.y - otherCity.y)**2)
-        
+
         roundedVal = int(round(initialVal))
-        
+
         return roundedVal
 
 #    Map
@@ -41,16 +41,16 @@ class Map:
     def __init__(self, inputFileName):
         self.cities = []
         self.loadCities(inputFileName)
-    
+
     # load's cities from input file
-    def loadCities(self,text):
+    def loadCities(self, text):
         _rawData = open(text,"r")
         _data = _rawData.read().splitlines()
-        
+
         for _line in _data:
             newCity = _line.split()
             self.cities.append(City(newCity[0], newCity[1], newCity[2]))
-    
+
     # function to print all cities
     # format: 'id' 'x' 'y'
     def printMap(self):
@@ -65,6 +65,6 @@ def distanceBetween(self,id1, id2):
 
 if __name__ == '__main__':
     inFile = sys.argv[1]
-    
+
     cities = Map(inFile)
     print cities.distanceBetween(5,29)
