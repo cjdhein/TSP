62a845a3924040116038a1041a993400ca473345
vchapple17 <vchapple17@gmail.com>
2017-11-22 10:25:03 -0600

    nearest neighbor basic solution
    Not good for large number of cities (too long to calculate)

--- NN/tsp-nn.py
+++ NN/tsp-nn.py
@@ -2,17 +2,52 @@
 
 # Val Chapple
 # Cody Dhein
+# Date: Nov 22, 2017
+
+# NearestNeighbor Driver program
+#
+# Greedy algorithm
+# Usage:
+# tsp-nn.py <filename> [optimal distance]
+# INPUTS:
+# filename:
+#   each line is a city with city Id, x-coord, y-coord
+# optimal distance: (WARNING: Only use for small number of cities)
+#   optional argument if optimal distance is known
+#   program compares distances of each city as a start city to the optimal solution
 
 import sys
 sys.path.insert(0, '../util')
 
 from tsp_utils import Map, City
+from nn import NearestNeighbor
 
 if __name__ == '__main__':
+
+    # Check input file name exists and is readable file
     try:
-        inFile = sys.argv[1]
+        filename = sys.argv[1]
     except:
         print("Usage: " + sys.argv[0] + " <inputfilename>")
         sys.exit()
 
+    try:
+        inFile = open(filename, "r")
+    except:
+        print("No file named: " + filename)
+        sys.exit()
+
+    # Create data structure
     cities = Map(inFile)
+    NN = NearestNeighbor(cities)
+
+    # Check each start city with optimal distance provided
+    # or calculate nearest neighbor distance from first city only
+    try:
+        optDistance = int(sys.argv[2])
+        NN.calculateAllStartingRoutes(optDistance)
+    except:
+        NN.calculateRoute(0)
+        fileWrite = open(filename + ".tour", "w")
+        NN.saveSolution(fileWrite)
+        fileWrite.close()
